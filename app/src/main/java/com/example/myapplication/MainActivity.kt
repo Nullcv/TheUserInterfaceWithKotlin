10e5f8340dec4950c0310d1c461fbe2bc1d0d955
Nullcv <87867393+Nullcv@users.noreply.github.com>
2023-02-11 18:22:29 -0500

    Referencing visual components inside MainActivity.kt, a solution to The 'kotlin-android-extensions' Gradle plugin is deprecated

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.myapplication
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.example.myapplication.databinding.ActivityMainBinding
 
+//import kotlinx.android.synthetic.main.activity_main.*
+private lateinit var binding: ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        addTextView("Hello")
+        addTextView("From")
+        addTextView("Kotlin")
+
+    }
+
+    private fun addTextView(label: String) {
+        val view = TextView(this)
+        view.text = label
+        view.textSize = 28f
+        view.setTextColor(Color.parseColor("#ff0000"))
+        binding.linearLayout.addView(view)
     }
 }
